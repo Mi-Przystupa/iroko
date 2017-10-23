a0b9548e8f2b15a5cfb06835481c3fed10639d5f
Fabian Ruffy <fabian.ruffy@gmail.com>
2017-10-22 18:27:33 -0700

    Add the ryu controller and bootstrap Iroko.

--- Hedera/DCController.py
+++ Hedera/DCController.py
@@ -5,10 +5,7 @@
     based on riplpox 
 '''
 
-import logging
-
 import sys
-sys.path.append('./')
 
 from struct import pack
 from zlib import crc32
@@ -18,18 +15,19 @@ import pox.openflow.libopenflow_01 as of
 
 from pox.lib.revent import EventMixin
 from pox.lib.util import dpidToStr
-from pox.lib.recoco import Timer
 from pox.lib.packet.ipv4 import ipv4
 from pox.lib.packet.udp import udp
 from pox.lib.packet.tcp import tcp
 
 from util import buildTopo, getRouting
 
+sys.path.append('./')
 
 log = core.getLogger()
 
 # Number of bytes to send for packet_ins
 MISS_SEND_LEN = 2000
+
 
 class Switch(EventMixin):
     def __init__(self):
@@ -42,29 +40,29 @@ class Switch(EventMixin):
             self.dpid = connection.dpid
         assert self.dpid == connection.dpid
         self.connection = connection
-    
-    def send_packet_data(self, outport, data = None):
-        msg = of.ofp_packet_out(in_port=of.OFPP_NONE, data = data)
-        msg.actions.append(of.ofp_action_output(port = outport))
+
+    def send_packet_data(self, outport, data=None):
+        msg = of.ofp_packet_out(in_port=of.OFPP_NONE, data=data)
+        msg.actions.append(of.ofp_action_output(port=outport))
         self.connection.send(msg)
-    
-    def send_packet_bufid(self, outport, buffer_id = -1):
+
+    def send_packet_bufid(self, outport, buffer_id=-1):
         msg = of.ofp_packet_out(in_port=of.OFPP_NONE)
-        msg.actions.append(of.ofp_action_output(port = outport)) 
+        msg.actions.append(of.ofp_action_output(port=outport))
         msg.buffer_id = buffer_id
         self.connection.send(msg)
-                        
-    def install(self, port, match, modify = False, buf = -1, idle_timeout = 0, hard_timeout = 0):
+
+    def install(self, port, match, modify=False, buf=-1, idle_timeout=0, hard_timeout=0):
         msg = of.ofp_flow_mod()
         msg.match = match
         if modify:
             msg.command = of.OFPFC_MODIFY_STRICT
-        else: 
+        else:
             msg.command = of.OFPFC_ADD
         msg.idle_timeout = idle_timeout
         msg.hard_timeout = hard_timeout
-        msg.actions.append(of.ofp_action_output(port = port))
-        #msg.buffer_id = buf          
+        msg.actions.append(of.ofp_action_output(port=port))
+        #msg.buffer_id = buf
         msg.flags = of.OFPFF_SEND_FLOW_REM
 
         self.connection.send(msg)
@@ -74,8 +72,9 @@ class Switch(EventMixin):
         # msg.type = of.OFPST_FLOW
         msg.body = of.ofp_flow_stats_request()
         #msg.body.match.in_port = port
-        self.connection.send(msg) 
- 
+        self.connection.send(msg)
+
+
 class DCController(EventMixin):
     def __init__(self, t, r):
         self.switches = {}  # [dpid]->switch
@@ -84,7 +83,7 @@ class DCController(EventMixin):
         self.r = r          # Routng object
         self.all_switches_up = False
         core.openflow.addListeners(self)
-    
+
     def _ecmp_hash(self, packet):
         ''' Return an ECMP-style 5-tuple hash for TCP/IP packets, otherwise 0.
         RFC2992 '''
@@ -106,45 +105,44 @@ class DCController(EventMixin):
         packet = event.parsed
         dpid = event.dpid
         in_port = event.port
-        t = self.t 
+        t = self.t
         # Broadcast to every output port except the input on the input switch.
         for sw_name in t.layer_nodes(t.LAYER_EDGE):
             for host_name in t.lower_nodes(sw_name):
                 sw_port, host_port = t.port(sw_name, host_name)
-                sw = t.node_gen(name = sw_name).dpid
+                sw = t.node_gen(name=sw_name).dpid
 
                 # Send packet out each non-input host port
                 if sw != dpid or (sw == dpid and in_port != sw_port):
                     self.switches[sw].send_packet_data(sw_port, event.data)
 
-
     def _install_reactive_path(self, event, out_dpid, final_out_port, packet):
         ''' Install entries on route between two switches. '''
-        in_name = self.t.node_gen(dpid = event.dpid).name_str()
-        out_name = self.t.node_gen(dpid = out_dpid).name_str()
+        in_name = self.t.node_gen(dpid=event.dpid).name_str()
+        out_name = self.t.node_gen(dpid=out_dpid).name_str()
         hash_ = self._ecmp_hash(packet)
-        route = self.r.get_route(in_name, out_name, hash_) 
+        route = self.r.get_route(in_name, out_name, hash_)
         if route is None:
             return
 
         match = of.ofp_match.from_packet(packet)
         for i, node in enumerate(route):
-            node_dpid = self.t.node_gen(name = node).dpid
+            node_dpid = self.t.node_gen(name=node).dpid
             if i < len(route) - 1:
                 next_node = route[i + 1]
                 out_port, next_in_port = self.t.port(node, next_node)
             else:
                 out_port = final_out_port
-            self.switches[node_dpid].install(out_port, match, idle_timeout = 10)
-        
-    def _handle_FlowStatsReceived (self, event):
+            self.switches[node_dpid].install(out_port, match, idle_timeout=10)
+
+    def _handle_FlowStatsReceived(self, event):
         pass
 
     def _handle_PacketIn(self, event):
         if not self.all_switches_up:
-            log.debug("Saw PacketIn before all switches were up - ignoring." )
+            log.debug("Saw PacketIn before all switches were up - ignoring.")
             return
-        
+
         packet = event.parsed
         dpid = event.dpid
         in_port = event.port
@@ -152,22 +150,21 @@ class DCController(EventMixin):
         # Learn MAC address of the sender on every packet-in.
         self.macTable[packet.src] = (dpid, in_port)
 
-    
         # Insert flow, deliver packet directly to destination.
         if packet.dst in self.macTable:
             out_dpid, out_port = self.macTable[packet.dst]
             self._install_reactive_path(event, out_dpid, out_port, packet)
-        
+
             self.switches[out_dpid].send_packet_data(out_port, event.data)
-            
+
         else:
             self._flood(event)
 
     def _handle_ConnectionUp(self, event):
         sw = self.switches.get(event.dpid)
         sw_str = dpidToStr(event.dpid)
-        sw_name = self.t.node_gen(dpid = event.dpid).name_str()
-        
+        sw_name = self.t.node_gen(dpid=event.dpid).name_str()
+
         if sw_name not in self.t.switches():
             log.warn("Ignoring unknown switch %s" % sw_str)
             return
@@ -183,17 +180,17 @@ class DCController(EventMixin):
 
         sw.connection.send(of.ofp_set_config(miss_send_len=MISS_SEND_LEN))
 
-        if len(self.switches)==len(self.t.switches()):
+        if len(self.switches) == len(self.t.switches()):
             log.info("All of the switches are up")
             self.all_switches_up = True
 
-def launch(topo = None, routing = None):
+
+def launch(topo=None, routing=None):
     if not topo:
-        raise Exception ("Please specify the topology")
-    else: 
+        raise Exception("Please specify the topology")
+    else:
         t = buildTopo(topo)
     r = getRouting(routing, t)
 
     core.registerNew(DCController, t, r)
     log.info("*** Controller is running")
-

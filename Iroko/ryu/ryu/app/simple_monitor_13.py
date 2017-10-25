d2d751e34d4b96903a4a9cf62b20b87b98cdee3b
Fabian Ruffy <fabian.ruffy@gmail.com>
2017-10-25 09:09:14 -0700

    Add traffic matrix to controller.

--- Iroko/ryu/ryu/app/simple_monitor_13.py
+++ Iroko/ryu/ryu/app/simple_monitor_13.py
@@ -142,7 +142,7 @@ class SimpleMonitor13(app_manager.RyuApp):
         while True:
             for dp in self.datapaths.values():
                 self._request_stats(dp)
-            hub.sleep(10)
+            hub.sleep(1)
 
     def _request_stats(self, datapath):
         self.logger.debug('send stats request: %016x', datapath.id)

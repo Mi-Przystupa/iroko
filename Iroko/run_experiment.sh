747dcbade5ca91a2d469849f81b5d47be5380593
Fabian Ruffy <fabian.ruffy@gmail.com>
2017-11-26 15:55:51 -0800

    Add some related work papers

--- Iroko/run_experiment.sh
+++ Iroko/run_experiment.sh
@@ -5,6 +5,15 @@ INPUT_FILES='stag_prob_0_2_3_data stag_prob_1_2_3_data stag_prob_2_2_3_data stag
 DURATION=60
 
 sudo mn -c
+
+for f in $INPUT_FILES;
+do
+        input_file=$INPUT_DIR/iroko/$f
+        pref="fattree-iroko"
+        out_dir=$OUTPUT_DIR/$pref/$f
+        sudo python iroko.py -i $input_file -d $out_dir -p 0.03 -t $DURATION --iroko
+done
+
 for f in $INPUT_FILES;
 do
         input_file=$INPUT_DIR/default/$f
@@ -27,14 +36,6 @@ do
         pref="fattree-dctcp"
         out_dir=$OUTPUT_DIR/$pref/$f
         sudo python iroko.py -i $input_file -d $out_dir -p 0.03 -t $DURATION --ecmp --dctcp
-done
-
-for f in $INPUT_FILES;
-do
-        input_file=$INPUT_DIR/iroko/$f
-        pref="fattree-iroko"
-        out_dir=$OUTPUT_DIR/$pref/$f
-        sudo python iroko.py -i $input_file -d $out_dir -p 0.03 -t $DURATION --iroko
 done
 
 for f in $INPUT_FILES;

7651230c9ce2687d02a866a8f4cc25a2429f8273
FRuffy <FRuffy@users.noreply.github.com>
2017-12-10 14:23:07 -0800

    Fix install

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+sudo apt-get install mininet
 sudo -H pip install pip --upgrade
 sudo -H pip install --upgrade numpy 
-sudo -H pip install http://download.pytorch.org/whl/cu75/torch-0.2.0.post3-cp27-cp27mu-manylinux1_x86_64.whl C
+sudo -H pip install http://download.pytorch.org/whl/cu75/torch-0.2.0.post3-cp27-cp27mu-manylinux1_x86_64.whl
 sudo -H pip install torchvision
 sudo apt-get install bwm-ng
 make -C Iroko/cluster_loadgen

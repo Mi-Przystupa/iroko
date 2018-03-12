e89ef116ae8da8368d2229e26e1907dd368d6284
Unknown <fabian.ruffy@gmail.com>
2018-03-12 13:11:32 -0700

    Update install.

--- install.sh
+++ install.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
+
+# fetch and install mininet
 sudo git submodule update --init --recursive
-sudo mininet/mininet/util/install.sh -nfv
+cd mininet
+sudo mininet/util/install.sh -nfv
+cd ..
 sudo -H pip install pip --upgrade
+# required for reinforcement learning
 sudo -H pip install --upgrade numpy 
 sudo -H pip install http://download.pytorch.org/whl/cu75/torch-0.2.0.post3-cp27-cp27mu-manylinux1_x86_64.whl
 sudo -H pip install torchvision
+# for generating plots
 sudo -H pip install matplotlib
+# traffic monitor
 sudo apt-get install -y bwm-ng
+# traffic generator
 make -C iroko/cluster_loadgen
+# reset accidental root files to the original user
+sudo chown -R $USER:$USER .
+

a42cdb22a0e7322295113bb3138c09c917ed899b
Unknown <fabian.ruffy@gmail.com>
2018-03-12 10:47:44 -0700

    Add mininet as submodule.
    Edit .gitmodules
    
    Fix install.

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-sudo Mininet/mininet/util/install.sh -nfv
+sudo git submodule update --init --recursive
+sudo mininet/mininet/util/install.sh -nfv
 sudo -H pip install pip --upgrade
 sudo -H pip install --upgrade numpy 
 sudo -H pip install http://download.pytorch.org/whl/cu75/torch-0.2.0.post3-cp27-cp27mu-manylinux1_x86_64.whl
